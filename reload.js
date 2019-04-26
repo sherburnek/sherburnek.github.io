8f67fde1913d75e138e175d6a3562a35f79663ee
Krysia Sherburne <sherburnek@sjabr.org>
2019-04-26 12:43:43 -0500

    changed reload function to add string to img url

--- reload.js
+++ reload.js
@@ -1,8 +1,13 @@
-$(document).ready(function() { /// Wait till page is loaded
+/*$(document).ready(function() { /// Wait till page is loaded
     setInterval(timingLoad, 30000);
     function timingLoad() {
     $('#img-container').load('#img-container', function() {
     console.log("The image div has reloaded.")
     });
     }
-    });
+    });*/
+$(document).ready(function(){
+    setInterval(function(){
+        $(".mosaic").attr("src", "https://sjabr.sharepoint.com/sites/GalaMosaic/Shared%20Documents/Mosaic-Master.jpg?"+new Date().getTime());
+    },10000);
+})
