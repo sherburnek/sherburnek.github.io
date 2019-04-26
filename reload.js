1f5b18ddedd75af4912318729cea045e9cd0966d
Krysia Sherburne <sherburnek@sjabr.org>
2019-04-26 12:57:58 -0500

    removed reload of div

--- reload.js
+++ reload.js
@@ -9,10 +9,5 @@
 $(document).ready(function(){
     setInterval(function(){
         $(".mosaic").attr("src", "https://sjabr.sharepoint.com/sites/GalaMosaic/Shared%20Documents/Mosaic-Master.jpg?"+new Date().getTime());
-    },60000, function(){
-        setInterval(timingLoad, 60000);
-        function timingLoad() {
-        $('#img-container').load('#img-container', function() {
-        console.log("The image div has reloaded.")  
-    });
+    },60000);
 })
