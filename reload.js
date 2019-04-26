7543834c0ad4c5d1bad4fc61ed15aea8bdcf6a53
Krysia Sherburne <sherburnek@sjabr.org>
2019-04-26 12:12:49 -0500

    updated load function

--- reload.js
+++ reload.js
@@ -1,7 +1,7 @@
 $(document).ready(function() { /// Wait till page is loaded
     setInterval(timingLoad, 30000);
     function timingLoad() {
-    $('#img-container').load('index.html #img-container', function() {
+    $('#img-container').load('#img-container', function() {
     console.log("The image div has reloaded.")
     });
     }
